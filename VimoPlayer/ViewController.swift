2210637970f0b1a5639a661d26750faea7e598a7
Ali <alikhajepur@gmail.com>
2023-04-02 13:49:51 +0330

    Initial Commit

---
+++ VimoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VimoPlayer
+//
+//  Created by Ali on 4/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
